8c5ca9cf6ad37fc0f14902088132360d4768531f
Ario Setiawan <consciousness.dev@gmail.com>
2019-04-05 23:35:36 +0700

    Launch Selfchat

---
+++ precache-manifest.94deb4fd50ec026e9aae3d828388b548.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8528e9f4834ed1d57fe",
+    "url": "/selfchat-v01/static/css/main.006a3ff2.chunk.css"
+  },
+  {
+    "revision": "c8528e9f4834ed1d57fe",
+    "url": "/selfchat-v01/static/js/main.c9910adb.chunk.js"
+  },
+  {
+    "revision": "3d7eb5a357d97abc4740",
+    "url": "/selfchat-v01/static/js/runtime~main.0c2d4a50.js"
+  },
+  {
+    "revision": "c06fa5518dbc2cc50139",
+    "url": "/selfchat-v01/static/js/2.1a16aee8.chunk.js"
+  },
+  {
+    "revision": "7e705a9103063bfafc534638568e4f60",
+    "url": "/selfchat-v01/index.html"
+  }
+];
